cdd7977109f3c8125c976015035704cfb54647e2
Lovepreet Singh Jassal <lovepreetjassal@lovepreet.local>
2023-10-13 13:02:53 -0400

    Added background command

--- SSH/remotePortForwarding.sh
+++ SSH/remotePortForwarding.sh
@@ -1,2 +1,5 @@
 #Forward traffic to local port 80 and from remote port 8080 @ server 'server'
+#Might need to add GatewayPorts yes to sshd_config
 ssh -R 0.0.0.0:8080:localhost:80 user@server
+# To run tunnel in background
+ssh -f -N -R 0.0.0.0:8080:localhost:80 user@server
