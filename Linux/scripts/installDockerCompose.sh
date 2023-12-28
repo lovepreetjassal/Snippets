1fc9534601095a4c5352589eb6f4fc37aa2ebfbc
Lovepreet Jassal <10718251-lovepreetjassal@users.noreply.gitlab.com>
2023-12-28 18:26:56 +0000

    Add new file installDockerCompose.sh

---
+++ Linux/scripts/installDockerCompose.sh
@@ -0,0 +1,3 @@
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.23.3/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
